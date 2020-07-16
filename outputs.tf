eb11f5e0dfd9f6088ff271b99cc9fc11a988aea6
Ritesh Kumar <riteshk.296@gmail.com>
2020-07-16 23:50:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,64 +1,37 @@
-output "mongo_deployment"{
-	value = kubernetes_deployment.mongo_deployment
+output "efs_cluster_dns_name"{
+	value= aws_efs_file_system.nfs_server.dns_name
 }
 
-output "mongo_secret" {
-	value = kubernetes_secret.mongo_secret
-} 
-
-output "mongo_pvc"{
-	value = kubernetes_persistent_volume_claim.mongo_pvc
+output "efs_cluster_id"{
+    value = aws_efs_file_system.nfs_server.id
 }
 
-output "mongo_svc"{
-	value = kubernetes_service.monogo_service
+
+output "efs_security_group_id"{
+	value = aws_security_group.efs_security_group.id
 }
 
-output "efs_storage_class"{
-	value = kubernetes_storage_class.efs_storage_class
+output "efs_storage_class_id"{
+	value = kubernetes_storage_class.efs_storage_class.id
 }
 
-output "efs_provisioner"{
-	value = kubernetes_deployment.efs_provisioner_deployment
+output "vpc_cidr_block"{
+	value = data.aws_vpc.vpc_details.cidr_block
 }
 
-output "vpc_details"{
-	value = data.aws_vpc.vpc_details
+output "subnet_ids"{
+	value = data.aws_subnet_ids.vpc_subnet_details.ids
 }
 
-output "efs_security_group"{
-	value = aws_security_group.efs_security_group
-}
-
-output "efs_mount_dns_name"{
-  value=aws_efs_mount_target.efs_mount_details[0]
-}
-
-output "efs"{
-	value= aws_efs_file_system.nfs_server
-}
-
-output "endpoint" {
+output "eks_cluster_endpoint" {
   value = aws_eks_cluster.eks_cluster.endpoint
 }
 
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.eks_cluster.certificate_authority.0.data
+
+output "eks_cluster_role_arn" {
+    value = aws_iam_role.eks_role.arn
 }
 
-output "eks_nodegroup"{
-  value= aws_eks_node_group.eks_node_group_2
+output "node_group_arn" {
+    value = aws_iam_role.node_group_role.arn
 }
-
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.eks_subnet : s.cidr_block]
-}
-
-output "subnet_details" {
-	value = data.aws_subnet.eks_subnet
-}
-
-output "vpc_id"{
-	value = data.aws_subnet_ids.vpc_details
-}
-
